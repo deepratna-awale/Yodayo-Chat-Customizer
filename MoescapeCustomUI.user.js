5211c4acee577358f998056d8b7f50e3435fd1ed
pervertir <hsbckp01@gmail.com>
2025-07-20 17:02:37 -0230

    Adds backgrounds to backgrounds.md

--- MoescapeCustomUI.user.js
+++ MoescapeCustomUI.user.js
@@ -1,45 +1,45 @@
-// ==UserScript==
-// @name         Yodayo/ Moescape Customizer
-// @version      ..1
+// 1.0.1
+// 1.0.1         Yodayo/ Moescape Customizer
+// 1.0.1      1.6.0
 
-// @namespace    MOESCAPE
+1.0.1// 1.0.1    MOESCAPE
 
-// @description  Fully Customize Yodayo/ Moescape Chats
-// @author       Pervertir
+1.0.1// 1.0.1  Fully Customize Yodayo/ Moescape Chats
+// 1.0.1       Pervertir
 
-// @homepageURL  https://github.com/pervertir/Yodayo-Chat-Customizer
-// @updateURL    https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/MoescapeCustomUI.user.js
-// @downloadURL  https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/MoescapeCustomUI.user.js
-// @supportURL   https://github.com/pervertir/Yodayo-Chat-Customizer/issues
+1.0.1// 1.0.1  https://github.com/pervertir/Yodayo-Chat-Customizer
+// 1.0.1    https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/MoescapeCustomUI.user.js
+// 1.0.1  https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/MoescapeCustomUI.user.js
+// 1.0.1   https://github.com/pervertir/Yodayo-Chat-Customizer/issues
 
-// @match        https://moescape.ai/*
-// @match        https://yodayo.com/*
+1.0.1// 1.0.1        https://moescape.ai/*
+// 1.0.1        https://yodayo.com/*
 
-// @run-at       document-end
-// @grant        GM_getResourceURL
-// @grant        GM_addStyle
-// @grant        GM_getResourceText
-// @grant        GM_xmlhttpRequest
+1.0.1// 1.0.1       document-end
+// 1.0.1        GM_getResourceURL
+// 1.0.1        GM_addStyle
+// 1.0.1        GM_getResourceText
+// 1.0.1        GM_xmlhttpRequest
 
-// @resource     chat_customizer_body        https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/chat_customizer_popup.html
-// @resource     customize_chat_button       https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/customize_chat_button.html
-// @resource     db_connect                  https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/db_connect_button.html
-// @resource     character_image_container   https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/character_image_container.html
-// @resource     image_viewer_popup          https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/image_viewer_popup.html
-// @resource     injection_notification      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/notification.html
-// @resource     card_layout                 https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/card_layout.html
-// @resource     pickr_css                https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/nano.min.css
+1.0.1// 1.0.1     chat_customizer_body        https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/chat_customizer_popup.html
+// 1.0.1     customize_chat_button       https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/customize_chat_button.html
+// 1.0.1     db_connect                  https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/db_connect_button.html
+// 1.0.1     character_image_container   https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/character_image_container.html
+// 1.0.1     image_viewer_popup          https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/image_viewer_popup.html
+// 1.0.1     injection_notification      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/notification.html
+// 1.0.1     card_layout                 https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/HTML/card_layout.html
+// 1.0.1     pickr_css                https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/nano.min.css
 
-// @require      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/constants.js
-// @require      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/database_handler.js
-// @require      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/utils.js
-// @require      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/ui_setters.js
-// @require      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/chat_customizer_popup.js
-// @require      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/image_viewer_popup.js
-// @require      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/moescape_chat_customizer.js
-// @require      https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js
+1.0.1// 1.0.1      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/constants.js
+// 1.0.1      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/database_handler.js
+// 1.0.1      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/utils.js
+// 1.0.1      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/ui_setters.js
+// 1.0.1      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/chat_customizer_popup.js
+// 1.0.1      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/image_viewer_popup.js
+// 1.0.1      https://github.com/pervertir/Yodayo-Chat-Customizer/raw/refs/heads/main/JS/moescape_chat_customizer.js
+// 1.0.1      https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js
 
-// @icon         https://moescape.ai/assets/images/logo.svg
+1.0.1// 1.0.1         https://moescape.ai/assets/images/logo.svg
 
-// @connect      *
-// ==/UserScript==
+1.0.1// 1.0.1      *
+// 1.0.1
